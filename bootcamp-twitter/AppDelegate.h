a7d342a03b628cba970b35bbe9f712463f23ba3b
PrashantKhanduri-local <prashant.khanduri@gmail.com>
2013-10-17 09:48:34 -0700

    Initial Commit

---
+++ bootcamp-twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bootcamp-twitter
+//
+//  Created by Prashant Khanduri - Hearsay on 10/17/13.
+//  Copyright (c) 2013 KDeal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
